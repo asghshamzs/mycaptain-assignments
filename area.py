35cfc2e91a99431b17154740a7b46065458566fd
m+ycaptain.py <101566894+asghshamzs@users.noreply.github.com>
2022-04-04 15:04:43 +0530

    area.py

--- area.py
+++ area.py
@@ -3,3 +3,8 @@ r=float(input('enter the radius circle :'))
 print("the radius of circle is :",r)
 area=pi*r*r
 print("the area of circle with radius",r,"is:",area)
+
+print the extension
+filename=input('input the filename':)
+f_extns=filename.split('.')
+print('the extension of file is:',f_extns[-1])
